2f99a025ba15d7a3c1224cdcdaded0fa47df8082
JesusContreras26 <jgcc26@gmail.com>
2024-11-20 13:48:51 -0400

    week 2 project

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,7 @@ const mongodb = require('../data/database');
 const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
+  //#swagger.tags = ['Users']
   const result = await mongodb.getDatabase().db().collection('users').find();
   result.toArray().then((users) => {
     res.setHeader('Content-Type', 'application/json');
@@ -10,6 +11,7 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
+  //#swagger.tags = ['Users']
   const userId = new ObjectId(req.params.id);
   const result = await mongodb
     .getDatabase()
@@ -22,7 +24,76 @@ const getSingle = async (req, res) => {
   });
 };
 
+const createUser = async (req, res) => {
+  //#swagger.tags = ['Users']
+  const user = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthDay: req.body.birthDay,
+  };
+  const result = await mongodb
+    .getDatabase()
+    .db()
+    .collection('users')
+    .insertOne(user);
+
+  if (result.acknowledged > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(result.error || 'Some error occurred while creating the user');
+  }
+};
+
+const updateUser = async (req, res) => {
+  //#swagger.tags = ['Users']
+  const userId = new ObjectId(req.params.id);
+  const user = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthDay: req.body.birthDay,
+  };
+  const result = await mongodb
+    .getDatabase()
+    .db()
+    .collection('users')
+    .replaceOne({ _id: userId }, user);
+
+  if (result.modifiedCount) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(result.error || 'Some error occurred while updating the user');
+  }
+};
+
+const deleteUser = async (req, res) => {
+  //#swagger.tags = ['Users']
+  const userId = new ObjectId(req.params.id);
+  const result = await mongodb
+    .getDatabase()
+    .db()
+    .collection('users')
+    .deleteOne({ _id: userId }, true);
+  if (result.deletedCount > 0) {
+    res.status(204).send();
+  } else {
+    res
+      .status(500)
+      .json(result.error || 'Some error occurred while updating the user');
+  }
+};
+
 module.exports = {
   getAll,
   getSingle,
+  createUser,
+  updateUser,
+  deleteUser,
 };
--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const mongodb = require('./data/database');
 const app = express();
+const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
 
 const port = process.env.PORT || 3000;
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(bodyParser.json());
 app.use('/', require('./routes'));
 
 mongodb.initDb((err) => {
@@ -11,7 +16,7 @@ mongodb.initDb((err) => {
     console.log(err);
   } else {
     app.listen(port, () => {
-      console.log(`Database is listening and node Running on ${port}`);
+      console.log(`Database is listening and node Running on port ${port}`);
     });
   }
 });
